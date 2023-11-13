479538b2b194d816faa3c2ba77b30a2d4c2287fb
francoisdouillet <francois.douillet@sfr.fr>
2023-11-13 12:04:22 +0100

    console.log

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ function myMap(arr, func) {
     return newArr
 }
 
+let array = [1, 6, 26, 29]
+console.log(myMap(array, x => x * x))
+
 function myFilter (arr, func) {
     let filteredArr = []
     for (i = 0 ; i < arr.length ; i++) {
         if(func(arr[i]) === true) {
-            filteredArr.push(func(arr[i]))
+            filteredArr.push(arr[i])
         }
     }
     return filteredArr
 }
+let array1 = [1, 6, 26, 29.5]
+console.log(myFilter(array1, x => x % 2 === 0))
 
 function myReduce (arr, func, initialValue) {
     let acc = initialValue
@@ -23,3 +28,8 @@ function myReduce (arr, func, initialValue) {
     }
     return acc
 }
+
+let array2 = [1, 6, 23, 39]
+console.log(myReduce(array2, function (currentValue, accumulator) {
+    return currentValue + accumulator;
+}, 0));
